20c8757b7617335c3bc954ad5376da11e8ffae47
Alexandra Castro <89374112+Alexcode88@users.noreply.github.com>
2021-11-09 01:45:58 -0600

    I was here

---
+++ src/main/java/com/codingdojo/app/repositories/NinjaRepository.java
@@ -0,0 +1,11 @@
+package com.codingdojo.app.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.codingdojo.app.models.Ninja;
+
+public interface NinjaRepository extends CrudRepository<Ninja, Long>{
+	List<Ninja> findAll();
+}
